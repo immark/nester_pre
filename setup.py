a6d1de696967d75aed6352751bdb568f321c917f
Mark <maanyng@yahoo.com.tw>
2015-07-15 16:12:26 +0800

    commit

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
         name = 'nester_pre',
         version = '1.4.2',
         py_modules =['nester'],
-        author ='mapython',
+        author ='Mark Ma',
         author_email = 'maanyng@yahoo.com.tw',
-        url = 'http://',
-        description = ' 這是清單列印函式, 將資料輸出至螢幕或指定對象, i.e.檔案',
+        url = 'https://github.com/immark/nester_pre',
+        description = ' A simple printer of nested list',
      )
